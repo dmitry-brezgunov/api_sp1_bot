578816256b74d9643e7624143c8dffe079eb5cae
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-03-25 16:25:35 +0300

    added proxy

--- homework.py
+++ homework.py
@@ -10,7 +10,8 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
+proxy = telegram.utils.request.Request(proxy_url='socks5://78.46.218.20:46764')
+bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 
 
 def parse_homework_status(homework):
